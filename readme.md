1b75ac4307a1159e38daec6aed02c77be5e95620
ruoningwen <ruoningwen@gmail.com>
2018-01-23 12:25:34 +0800

    add requirements and update readme

--- readme.md
+++ readme.md
@@ -1,4 +1,15 @@
 # SIF_YIMIAN
 
-This is the implement of the sentence embedding algorithm in [the paper](https://openreview.net/forum?id=SyK00v5xx) "A Simple but Tough-to-Beat Baseline for Sentence Embeddings" for Yimian.
+This is the implement of the sentence embedding algorithm in [the paper](https://openreview.net/forum?id=SyK00v5xx) "A Simple but Tough-to-Beat Baseline for Sentence Embeddings" in Python3 and in Chinese corpus.
+
+
+## Install
+
+```angular2html
+$ pip install -r requirements.txt
+```
+
+## Get started
+To get started, just cd into the directory src/ and run sif_embedding.py.
+
 
--- src/sif_embedding.py
+++ src/sif_embedding.py
@@ -6,9 +6,20 @@ import gensim
 from sklearn.decomposition import TruncatedSVD
 from scipy import spatial
 
+# set the word and sentence dimension
+tea_w2v_size = 200
+
+# get the word2vec model
 tea_w2v_model = gensim.models.Word2Vec.load('/data/w2v_model/tea/with_wiki.model')
+
+# get the p(w) dict
 tea_dict_word_weight = pickle.load(open('/data/sif_model/dict_word_weight.p', 'rb'))
-w2v_size = 200
+
+# get the sif_embedding of sentences
+tea_sif_embedding = pickle.load(open('/data/sif_model/sif_embedding.p', 'rb'))
+
+# get the principle component
+tea_pc = pickle.load(open('/data/sif_model/pc.p', 'rb'))
 
 # get the sentences
 tea_sentences = []
@@ -16,12 +27,6 @@ with open('/data/sif_model/tea_question.csv') as f:
     for line in f.readlines():
         line = line.replace('\n', '')
         tea_sentences.append(line)
-
-# get the sif_embedding of sentences
-sif_embedding = pickle.load(open('/data/sif_model/sif_embedding.p', 'rb'))
-
-# get the principle component
-pc = pickle.load(open('/data/sif_model/pc.p', 'rb'))
 
 
 def get_weighted_embedding(sentences, w2v_model, dict_word_weight, w2v_size):
@@ -65,11 +70,13 @@ def remove_pc(X, npc=1):
     return XX
 
 
+# return the cosine similarity of v1 and v2
 def my_cosine_similarity(v1, v2):
     return 1 - spatial.distance.cosine(v1, v2)
 
 
-def get_sent_embedding(text, pc, w2v_model, dict_word_weight):
+def get_sent_embedding(text, pc, w2v_model, dict_word_weight, w2v_size):
+    # init the sentence embedding
     s_embedding = np.array([0.0] * w2v_size)
     words = list(jieba.cut(text))
     for word in words:
@@ -77,13 +84,13 @@ def get_sent_embedding(text, pc, w2v_model, dict_word_weight):
             s_embedding += w2v_model[word] * dict_word_weight[word]
         else:
             s_embedding += [1] * w2v_size
+    # remove the principle component from the sentence
     rmpc_s_embedding = s_embedding - s_embedding.dot(pc.transpose()) * pc
-    embedding = rmpc_s_embedding
-    return embedding
+    return rmpc_s_embedding
 
 
-def get_most_similar_k(test_str, k, sentences, sentences_emb, w2v_model, dict_word_weight):
-    sent_emb = get_sent_embedding(test_str, pc, w2v_model, dict_word_weight)
+def get_most_similar_k(test_str, k, sentences, sentences_emb, w2v_model, dict_word_weight, pc, w2v_size):
+    sent_emb = get_sent_embedding(test_str, pc, w2v_model, dict_word_weight, w2v_size)
     distance_list = np.array(list(map(lambda x: my_cosine_similarity(sent_emb, x), sentences_emb)))
     smallest_k_index = distance_list.argsort()[::-1][:k]
     for i in smallest_k_index:
@@ -91,7 +98,6 @@ def get_most_similar_k(test_str, k, sentences, sentences_emb, w2v_model, dict_wo
 
 
 if __name__ == '__main__':
-
     # print(len(sentences))
     # print(sentences[:2])
     # get the weighted embedding
@@ -110,4 +116,4 @@ if __name__ == '__main__':
     # pickle.dump(sif_embedding, open('../data/sif_embedding.p', 'wb'))
 
     test_sent = '是不是等级越高的茶越好'
-    get_most_similar_k(test_sent, 5, tea_sentences, sif_embedding)
+    get_most_similar_k(test_sent, 5, tea_sentences, tea_sif_embedding, tea_w2v_model, tea_dict_word_weight, tea_pc, tea_w2v_size)
